5d487e0947b44af481558d51cc3edfca6d9ad14a
shivani-1702 <shivmish888@gmail.com>
2020-12-02 14:33:38 +0530

    payment gateway integration reversal transfer via code

--- paymentgateway/pg/urls.py
+++ paymentgateway/pg/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index),
     path('response/', views.order, name="response"),
     path('payment/', views.payment, name="payment"),
+    path('reverse/', views.reverse, name="reverse"),
     path(r'^done/', views.manual, name="done")
 ]
--- paymentgateway/pg/views.py
+++ paymentgateway/pg/views.py
@@ -1,5 +1,10 @@
+import time
+
 import razorpay
 from django.shortcuts import render
+from django.template import RequestContext
+from django.template.context_processors import csrf
+
 from .models import Orders
 from django.http import HttpResponse
 
@@ -25,15 +30,39 @@ def order(request):
         order = Orders(name=name, address=address, email=email, contact=contact, amount=amount)
         #print(order.__dict__)
         amount_inr = int(amount)*100
+        transfers= [
+            {
+                "account": "acc_G6Arrf7RMyX7Rf",
+                "amount": 1000,
+                "currency": "INR",
+                "notes": {
+                    "branch": "Acme Corp Bangalore North",
+                    "name": "Gaurav Kumar"
+                },
+                "linked_account_notes": [
+                    "branch"
+                ],
+                "on_hold": 1,
+                "on_hold_until": 1671222870
+            }
+        ]
+
+        # context = RequestContext(request)
+        # context_dict = {}
+        # context_dict.update(csrf(request))
+        # print(context_dict, "----00000-----")
+
         dict={'order_id':"1", 'adress':address} #15 key value pairs optional, if merchant wants to send anything additional
 
         #DATA={'amount':amount_inr, 'currency':'INR', 'receipt':order.order_id, 'payment_capture':'1', 'notes':dict}
-        DATA = {'amount': amount_inr, 'currency': 'INR', 'receipt': order.order_id,'payment_capture':'0',
-                'notes': dict}
+        DATA = {'amount': amount_inr, 'currency': 'INR', 'receipt': order.order_id,'payment_capture':'1',
+                'notes': dict, 'transfers':transfers}
+        # DATA = {'amount': amount_inr, 'currency': 'INR', 'receipt': order.order_id, 'payment_capture': '1',
+        #                'notes': dict}
 
         order_created= client.order.create(data=DATA)
 
-        print(order_created, type(order_created), "order created-----")
+        print(order_created, type(order_created), "-----order created-----")
         global order_id_server, amount_paid
         order_id_server= order_created.get('id')
         #order_id_server = client.order.fetch('id')
@@ -45,6 +74,8 @@ def order(request):
         'currency': order_created.get('currency'),
         'key_id':key_id,
         'key_secret':key_secret}
+
+    #return render(request, 'pg/callback.html', para, context_dict.get('csrf_token'))
     return render(request, 'pg/handlerfunc.html', para)
     #return render(request, 'pg/paymentpage.html', para)
     #return render(request, 'pg/manual_chcekout.html', para)
@@ -52,6 +83,7 @@ def order(request):
 
 def payment(request):
     #payment notes, manual chcekout
+
     if request.method=='POST':
         payment_id = request.POST.get('razorpay_payment_id')
         print(payment_id, "-----")
@@ -62,6 +94,32 @@ def payment(request):
             'razorpay_payment_id': payment_id,
             'razorpay_signature': signature
         }
+        data = {'payment_id':'pay_G7nH5vQ10tl7S7'
+                }
+
+        transfers = client.transfer.all(data)
+        print(transfers)
+        print(dict(transfers), "--------------transfers dict--------")
+        data={'payment_id':payment_id,
+              'transfers': [
+            {
+                "account": "acc_G6Arrf7RMyX7Rf",
+                "amount": 1000,
+                "currency": "INR",
+                "notes": {
+                    "branch": "Acme Corp Bangalore North",
+                    "name": "Gaurav Kumar"
+                },
+                "linked_account_notes": [
+                    "branch"
+                ],
+                "on_hold": 1,
+                "on_hold_until": 1671222870
+            }
+        ]}
+        transfers= client.transfer.create(data)
+        print("=====transfer===", transfers)
+
         verified= client.utility.verify_payment_signature(params_dict) #None
         print(verified, "----Verified---")
         user_details ={
@@ -78,3 +136,19 @@ def manual(request):
     else:
         message = "Not Ajax"
     return HttpResponse(message)
+
+def reverse(request):
+    if request.method=='POST':
+        #time.sleep(30)
+        print(request, "=====request=====")
+        payment_id = request.POST.get('payment_id')
+        print(payment_id, "=====payment_id====")
+        data={'payment_id': payment_id}
+        a=client.transfer.all(data)
+        for i in a['items']:
+            print(i, "--------")
+        data={"amount": 100,
+            "reverse_all": 1}
+        a=client.transfer.reverse(payment_id, data)
+        print(a)
+    return render(request, HttpResponse('OKAy...'))
